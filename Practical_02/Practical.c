6423f92b3b02453f61c82aaea5b20756c69ecef3
aryannagvekar <aryannagvekar527@gmail.com>
2026-02-23 12:01:18 +0530

    Upload Practical_02

---
+++ Practical_02/Practical.c
@@ -0,0 +1,205 @@
+Program 1: Design a web page with a form that uses all types of controls.
+
+
+Code:
+
+
+<html>
+
+<head><title>
+
+form</title>
+
+</head>
+
+<body>
+
+<form>
+
+<label for="Name">Name:</label>
+
+<input type="text" name="text"></br></br>
+
+<label for="Username">Username:</label>
+
+<input type="text" name="name"></br></br>
+
+<label for="Password">Password:</label>
+
+<input type="password" name="pass"></br></br>
+
+<label for="email">Email:</label>
+
+<input type="email" name="email"></br></br>
+
+<label for="gender">Gender:</label></br>
+
+<input type="radio" name="gen"
+
+value="Male">Male
+
+<input type="radio" name="gen"
+
+value="Female">Female</br></br>
+
+<label for="subject">Subject:</label></br>
+
+<input type="checkbox" name="sub"
+
+value="MATHS">Maths</br>
+
+<input type="checkbox" name="sub"
+
+value="Science">Science</br>
+
+<input type="checkbox" name="sub"
+
+value="History">History</br>
+
+<input type="checkbox" name="sub"
+
+value="English">English</br>
+
+<label for="city">City</label></br>
+
+<select name="dropdown">
+
+<option
+
+value="Mumbai"selected>Mumbai</option>
+
+<option value="ratnagiri">Ratnagiri</option> Output(s):
+
+<option value="pune">Pune</option> <option value="kohlapur">Kohlapur</option> </select></br></br> <input type="submit" value="Submit"> <input type="reset" value="Reset"> </from> </body> </html>
+
+Program 2: Design an admission form for any course in your college with text, password fields, check
+
+boxes, radio button and reset button.
+
+
+Code:
+
+
+<html>
+
+<head><title>
+
+form</title>
+
+</head>
+
+<body>
+
+<form>
+
+<label for="Name">Name:</label>
+
+<input type="text" name="text"></br></br>
+
+<label for="Username">Username:</label>
+
+<input type="text" name="name"></br></br>
+
+<label for="Password">Password:</label>
+
+<input type="password" name="pass"></br></br>
+
+<label for="email">Email:</label>
+
+<input type="email" name="email"></br></br>
+
+<label for="gender">Gender:</label></br>
+
+<input type="radio" name="gen"
+
+value="Male">Male
+
+<input type="radio" name="gen"
+
+value="Female">Female</br></br>
+
+<label for="subject">Subject:</label></br>
+
+<input type="checkbox" name="sub"
+
+value="MATHS">HTML</br>
+
+<input type="checkbox" name="sub"
+
+value="Science">C++</br>
+
+<input type="checkbox" name="sub"
+
+value="History">JAVA</br>
+
+<input type="submit" value="Submit">
+
+<input type="reset" value="Reset">
+
+
+</from>
+
+</body> Output(s):
+
+</html>
+
+Program 3: Write a program to get the following output:
+
+
+
+Code:
+
+
+<html>
+
+<head><title>
+
+form</title>
+
+</head>
+
+<body>
+
+<table border="0">
+
+<form>
+
+<tr>
+
+<th colspan="2">SIGN</th>
+
+</tr>
+
+<tr>
+
+<td>Email address</td>
+
+<td><input type="email" name="email"></td>
+
+</tr>
+
+<tr>
+
+<td>Password</td>
+
+<td><input type="password"
+
+name="password"></td>
+
+</tr>
+
+<tr>
+
+<td colspan="2" align="center"><input
+
+type="submit" value="SIGN IN"></td>
+
+</tr>
+
+</from>
+
+</table>
+
+</body>
+
+</html>
